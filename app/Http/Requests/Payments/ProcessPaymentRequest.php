7119ccad997cd1053a125d2ad5de672fe88c26df
elgamal <ahmedelgamal5320@gmail.com>
2026-06-26 23:08:23 +0300

    refact: ProcessPaymentRequest to use PaymentGatewayManager directly for available payment methods

--- app/Http/Requests/Payments/ProcessPaymentRequest.php
+++ app/Http/Requests/Payments/ProcessPaymentRequest.php
@@ -7,6 +7,12 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ProcessPaymentRequest extends FormRequest
 {
+    public function __construct(
+        private readonly PaymentGatewayManager $gatewayManager,
+    ) {
+        parent::__construct();
+    }
+
     public function authorize(): bool
     {
         return true;
@@ -16,12 +22,7 @@ class ProcessPaymentRequest extends FormRequest
     {
         return [
             'order_id' => ['required', 'integer', 'exists:orders,id'],
-            'method' => ['required', 'string', 'in:'.implode(',', $this->availableMethods())],
+            'method' => ['required', 'string', 'in:'.implode(',', $this->gatewayManager->getAvailableMethods())],
         ];
-    }
-
-    private function availableMethods(): array
-    {
-        return app(PaymentGatewayManager::class)->getAvailableMethods();
     }
 }
